cb6f983740c044e59aebb276fc3d545f24cf631a
DaCodeChick <peepvtuber@gmail.com>
2025-05-23 15:29:33 -0700

    enBitTst

--- common/util.cpp
+++ common/util.cpp
@@ -3,6 +3,20 @@
 // manord: 080a312c
 static short gLastRnd = 0;
 
+// manord: 08048204
+int enBitTst(int value, short bit)
+{
+	short sVar1;
+
+	sVar1 = bit;
+	if (bit < 0)
+	{
+		sVar1 = bit + 7;
+	}
+	return (uint)(((int)*(char *)((sVar1 >> 3) + value) &
+	               0x80 >> ((char)bit - (char)((sVar1 >> 3) << 3) & 0x1fU)) != 0);
+}
+
 // manord: 08048124
 short enRandom(short seed, short bound)
 {
--- common/util.h
+++ common/util.h
@@ -2,4 +2,5 @@
 
 #include "typedefs.h"
 
+int enBitTst(int value, short bit);
 short enRandom(short seed, short bound);
